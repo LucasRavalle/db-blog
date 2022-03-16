36864a140603dcdeb8d7c04bf261c9d8af7c06d4
LucasRavalle <ravallelucas@gmail.com>
2022-03-16 00:49:37 -0300

    Agregar archivos de proyecto.

---
+++ Blog/BlogDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-G800P7O\\SQLEXPRESS;Database=blog;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Blog/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
+        //reference navigation properties
+        public User User { get; set; }
+
+    }
+}
